c999c6f29dccc198d76436d155d695c82097a5e9
Dominik Stenka <dominik.stenka2309@outlook.com>
2023-04-07 17:25:44 +0200

    Add project files.

---
+++ Authentication/Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using Authentication.DTOs;
+using Authentication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Authentication.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IConfiguration configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public static User user = new();
+
+    [HttpPost("register")]
+    public ActionResult<User> Register(UserDTO request)
+    {
+        CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+        user.UserName = request.UserName;
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+
+        return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public ActionResult<string> Login(UserDTO request)
+    {
+        if (user.UserName != request.UserName)
+            return BadRequest("Invalid credentials");
+
+        if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            return BadRequest("Invalid credentials");
+
+        string token = CreateToken(user);
+        return Ok(token);
+    }
+
+    private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using var hmac = new HMACSHA512();
+
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+    }
+
+    private static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using var hmac = new HMACSHA512(passwordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        return computedHash.SequenceEqual(passwordHash);
+    }
+
+    private string CreateToken(User user)
+    {
+        List<Claim> claims = new()
+        {
+            new Claim(ClaimTypes.Name, user.UserName)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("Jwt:Key").Value));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: creds);
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
+    }
+}
---
+++ Authentication/DTOs/UserDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Authentication.DTOs;
+
+public class UserDTO
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
